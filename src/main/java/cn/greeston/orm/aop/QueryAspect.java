4d2d8df916d0373f2e01d4343a4bfa8319403f20
LayJiang <doctor_2012_jiang@163.com>
2017-09-06 15:51:12 +0800

    greeston-orm 首次提交

---
+++ src/main/java/cn/greeston/orm/aop/QueryAspect.java
@@ -0,0 +1,42 @@
+package cn.greeston.orm.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+
+import cn.greeston.orm.annotation.QueryPage;
+
+
+@Aspect
+public class QueryAspect implements Ordered, InitializingBean {
+
+    @Autowired(required = false)
+    private SqlSessionTemplate sqlSessionTemplate;
+
+    public void afterPropertiesSet() throws Exception {
+        System.out.println();
+    }
+
+    public int getOrder() {
+        return 20;
+    }
+
+    @Around("this(cn.greeston.orm.dao.BaseDao)")
+    public Object doQuery(ProceedingJoinPoint pjp) throws Throwable {
+
+        MethodSignature ms = (MethodSignature) pjp.getSignature();
+        QueryPage queryPageCount = ms.getMethod().getAnnotation(QueryPage.class);
+
+        if (queryPageCount != null) {
+            return new CustomPageProcess(sqlSessionTemplate).process(pjp);
+        } else {
+            return pjp.proceed(pjp.getArgs());
+        }
+    }
+}
+
